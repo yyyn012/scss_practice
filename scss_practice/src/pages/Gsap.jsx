1f389bb114d7e3ccb5e843e001fe9f33f62d7dd1
yyyn012 <yyyn12_@naver.com>
2024-09-19 15:46:35 +0900

    pages

---
+++ scss_practice/src/pages/Gsap.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Gsap = () => {
+  return <div>Gsap</div>;
+};
+
+export default Gsap;
---
+++ scss_practice/src/pages/Not.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Not = () => {
+  return <div>Not</div>;
+};
+
+export default Not;
